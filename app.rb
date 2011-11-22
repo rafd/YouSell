727d7ceac2713561cbbffe4971a23edba42b7070
Rafal Dittwald <rafal.dittwald@gmail.com>
2011-11-22 15:14:48 -0500

    login working

--- app.rb
+++ app.rb
@@ -1,17 +1,32 @@
 require 'sinatra'
 
 class App < Sinatra::Base
+  set :root, File.dirname(__FILE__)
+
+  $USERS = {}
+
   get '/' do
+    cookie = request.cookies["user"]
+    @user = $USERS[cookie] if(cookie and cookie.length > 1)
+
     erb :index
   end
 
-  post '/register' do
-
-  end
-
-
   post '/login' do
+    name = params["name"]
+    pass = params["password"]
 
+    if $USERS[name]
+      if $USERS[name][:pass] == pass
+        response.set_cookie "user", name
+      end
+    else
+      $USERS[name] = {:name=> name, :pass => pass, :balance => 50}
+
+      response.set_cookie "user", name
+    end
+
+    redirect '/'
   end
 
 
